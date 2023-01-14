bc10349c0935b065804acee5932dd2e6f1455fab
Marcelo Doz <marcelo_doz@outlook.com>
2023-01-14 01:06:55 -0300

    Add order placer

--- main.go
+++ main.go
@@ -3,15 +3,53 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
-func main() {
+type OrderPlacer struct {
+	producer   *kafka.Producer
+	topic      string
+	deliverych chan kafka.Event
+}
 
+func NewOrderPlacer(p *kafka.Producer, topic string) *OrderPlacer {
+	return &OrderPlacer{
+		producer:   p,
+		topic:      topic,
+		deliverych: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *OrderPlacer) placeOrder(orderType string, size int) error {
+	var (
+		format  = fmt.Sprintf("%s - %d", orderType, size)
+		payload = []byte(format)
+	)
+
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &op.topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: payload,
+	},
+		op.deliverych,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-op.deliverych
+	return nil
+}
+
+func main() {
+	topic := "test-topic"
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
-		"client.id":         "something",
+		"client.id":         "foo",
 		"acks":              "all",
 	})
 
@@ -19,22 +57,38 @@ func main() {
 		fmt.Printf("Failed to create producer: %s\n", err)
 	}
 
-	deliverch := make(chan kafka.Event, 10000)
-	topic := "test-topic"
-	err = p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: 0},
-		Value:          []byte("Foo"),
-	},
-		deliverch,
-	)
+	go func() {
+		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers": "localhost:9092",
+			"group.id":          "foo",
+			"auto.offset.reset": "smallest",
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	if err != nil {
-		log.Fatal(err)
+		err = consumer.Subscribe(topic, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			ev := consumer.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("consumed message from the queue: %s\n", string(e.Value))
+			case *kafka.Error:
+				fmt.Printf("%s\n", e)
+			}
+		}
+	}()
+
+	op := NewOrderPlacer(p, topic)
+
+	for i := 0; i < 1000; i++ {
+		if err := op.placeOrder("market", i+1); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Second * 3)
 	}
-
-	e := <-deliverch
-
-	fmt.Printf("%v\n", e.String())
-
-	fmt.Printf("%v\n", p.String())
 }
